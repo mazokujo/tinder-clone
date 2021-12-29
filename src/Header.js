65b3493be826d6f61552ce227346c5dbbd6dd889
mazokujo <kabasele.jonathan@yahoo.fr>
2021-12-29 13:40:58 +0200

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import './Header.css'
+import PersonIcon from '@material-ui/icons/Person'
+import IconButton from '@material-ui/core/IconButton'
+import ForumIcon from '@material-ui/icons/Forum'
+function Header() {
+
+    return (
+        <div className='header' >
+            <IconButton>
+                <PersonIcon fontSize='large' className='header__icon' />
+            </IconButton>
+
+            <IconButton>
+                <img
+                    className='header__logo'
+                    src='https://cdn.worldvectorlogo.com/logos/tinder-icon.svg'>
+                </img>
+            </IconButton>
+            <IconButton>
+                <ForumIcon fontSize='large' className='header__icon' />
+            </IconButton>
+
+        </div>
+    )
+}
+
+export default Header
